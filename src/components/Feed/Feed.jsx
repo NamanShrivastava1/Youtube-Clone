1fcaf663a03a72832ad1a8ae1c74bddbdea25510
Naman Shrivastava <namanshrivastava008@gmail.com>
2025-03-12 15:57:38 +0530

    Main Code

---
+++ src/components/Feed/Feed.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import { API_KEY, value_converter } from "../../data";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import moment from "moment";
+import "./Feed.css";
+
+const Feed = ({ category }) => {
+  const [data, setData] = useState([]);
+
+  // Another Way of Fetching Data
+  // const fetchData = async () => {
+  //   const videoList_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`;
+  //   await fetch(videoList_url)
+  //     .then((response) => response.json())
+  //     .then((data) => setData(data.items));
+  // };
+
+  const fetchData = async () => {
+    const response = await axios.get(
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=100&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`
+    );
+    setData(response.data.items);
+  };
+  console.log(data);
+
+  useEffect(() => {
+    fetchData();
+  }, [category]);
+
+  return (
+    <div className="feed">
+      {data.map((item, index) => {
+        return (
+          <Link
+            to={`video/${item.snippet.categoryId}/${item.id}`}
+            className="card"
+          >
+            <img src={item.snippet.thumbnails.medium.url} alt="" />
+            <h2>{item.snippet.title}</h2>
+            <h3>{item.snippet.channelTitle}</h3>
+            <p>
+              {value_converter(item.statistics.viewCount)} &bull;{" "}
+              {moment(item.snippet.publishedAt).fromNow()}
+            </p>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Feed;
